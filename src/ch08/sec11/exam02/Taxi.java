ac30edde2c9ff14fed649fec6acf05a831f4e6f7
taewon1918 <taewon1918@gmail.com>
2025-06-20 15:39:20 +0900

    06.20 객체 생성

--- src/ch08/sec11/exam02/Taxi.java
+++ src/ch08/sec11/exam02/Taxi.java
@@ -3,16 +3,17 @@ package ch08.sec11.exam02;
 public class Taxi implements Vehicle, Trunk {
     @Override
     public void run() {
-
-    }
-
-    @Override
-    public void load(String str) {
+        System.out.println("택시가 달린다.");
 
     }
 
     @Override
     public void stop() {
+        System.out.println("택시가 멈춘다.");
+    }
 
+    @Override
+    public void load(String str) {
+        System.out.printf("택시가 %s\n" );
     }
 }
