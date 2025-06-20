d1cfa23b19ac8c61af38180c02887aa758816402
taewon1918 <taewon1918@gmail.com>
2025-06-20 18:06:37 +0900

    06.20 수업내용

---
+++ src/control/ioc/SamsungTv.java
@@ -0,0 +1,22 @@
+package control.ioc;
+
+import control.Speaker;
+import control.Tv;
+public class SamsungTv implements Tv {
+    private Speaker speaker;
+
+    public SamsungTv(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("SamsungTv 의 소리는요");
+        this.speaker.speakerSound();
+    }
+    // 삼성Tv는 스피커를 사용할 수 있다.
+    // 스피커는 HarmanSpeaker를 사용한다.
+
+
+
+}
---
+++ src/control/legacy/SamsungTv.java
@@ -0,0 +1,24 @@
+package control.legacy;
+
+import control.Speaker;
+import control.Tv;
+
+
+public class SamsungTv implements Tv {
+    private Speaker speaker;
+
+    public SamsungTv() {
+        this.speaker = new JBLSpeaker();
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("SamsungTv 의 소리는요");
+        this.speaker.speakerSound();
+    }
+    // 삼성Tv는 스피커를 사용할 수 있다.
+    // 스피커는 HarmanSpeaker를 사용한다.
+
+
+
+}
